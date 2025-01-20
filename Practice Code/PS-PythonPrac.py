ffe9cf1b7fe98e586ba17c11a7fc4a66727a6bc8
Hibiscus4431 <159493880+Hibiscus4431@users.noreply.github.com>
2025-01-20 00:38:27 -0600

    Update PS-PythonPrac.py

--- Practice Code/PS-PythonPrac.py
+++ Practice Code/PS-PythonPrac.py
@@ -1,3 +1,22 @@
 #Paige Smith
 
 #Practice code for Python
+#write a basic class outline, and a function to print something 
+
+#class outline
+class Student:
+    #constructor
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    #function to print student info
+    def printStudent(self):
+        print("Name: " + self.name)
+        print("Age: " + str(self.age))
+        print("Grade: " + str(self.grade))
+
+#student object
+janeDoe = Student("Jane Doe", 16, 10)
+janeDoe.printStudent()
