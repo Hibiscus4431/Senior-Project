3c4072c4ed89129f41f9f55a63b5960117bdbd70
stuckeyrj <65870783+stuckeyrj@users.noreply.github.com>
2025-02-02 20:48:34 -0600

    Update Attachments_MetaData.sql

--- Backend/Migrations/Attachments_MetaData.sql
+++ Backend/Migrations/Attachments_MetaData.sql
@@ -2,5 +2,5 @@ CREATE TABLE Attachments_MetaData (
     id SERIAL PRIMARY KEY,
     attachment_id INT NOT NULL,
     reference_id INT NOT NULL,
-    reference_type VARCHAR(20) CHECK (reference_type IN ('question', 'text')) NOT NULL
+    reference_type VARCHAR(20) CHECK (reference_type IN ('question', 'test')) NOT NULL
     );
