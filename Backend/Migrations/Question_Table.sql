147e84ea036481f94792caf0494da1a98ac9d9a2
stuckeyrj <65870783+stuckeyrj@users.noreply.github.com>
2025-02-05 13:54:56 -0600

    Update Question_Table.sql
    added true or false section replacing correct answer because a new table will be added for the multiple choice

--- Backend/Migrations/Question_Table.sql
+++ Backend/Migrations/Question_Table.sql
@@ -1,15 +1,15 @@
 CREATE TABLE Questions (
-    question_id SERIAL PRIMARY KEY,
-    course_id INT NOT NULL,
-    question_text TEXT NOT NULL
+    id SERIAL PRIMARY KEY,
+    course_id INT DEFAULT NULL,
+    textbook_id INT DEFAULT NULL,
+    question_text TEXT NOT NULL,
     type VARCHAR(50) CHECK (type IN ('Multiple Choice', 'True/False', 'Short Answer', 'Essay')),
-    options JSON,
-    correct_answer TEXT,
+    true_false_answer BOOLEAN DEFAULT NULL,
     default_points INT DEFAULT 0,
     est_time INT,
     grading_instructions TEXT,
     owner_id INT NOT NULL,
     is_published BOOLEAN DEFAULT FALSE,
-    attachment_id INT  
+    attachment_id INT DEFAULT NULL,  
     source VARCHAR(50) CHECK (source IN ('manual', 'canvas_qti')) DEFAULT 'manual'
 );
