73bcd4e2c2e5442f1caedcb34750286878de05ee
stuckeyrj <65870783+stuckeyrj@users.noreply.github.com>
2025-02-05 14:35:07 -0600

    Update Users_Table.sql
    updated password for hashing capabilities

--- Backend/Migrations/Users_Table.sql
+++ Backend/Migrations/Users_Table.sql
@@ -4,5 +4,7 @@ CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
     role user_role NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password BYTEA NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
 );
