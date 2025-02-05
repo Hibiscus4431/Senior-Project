f93c6ee8e7c5797b2969afb6a5c30b292b45f9d5
stuckeyrj <65870783+stuckeyrj@users.noreply.github.com>
2025-02-05 14:57:56 -0600

    Update Attachments_Table.sql
    Got rid of the type variable becasue it was not serving a purpose for the project

--- Backend/Migrations/Attachments_Table.sql
+++ Backend/Migrations/Attachments_Table.sql
@@ -2,5 +2,4 @@ CREATE TABLE Attachments (
     attachments_id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
     filepath VARCHAR(255) NOT NULL,
-    type VARCHAR(50) -- Got to figure out how this works   
 );
