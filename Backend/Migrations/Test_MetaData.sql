b071a86d702409950eede8bca574fcedd3fd7ccb
stuckeyrj <65870783+stuckeyrj@users.noreply.github.com>
2025-02-05 14:24:39 -0600

    Update Test_MetaData.sql
    deleted id for test meta because it is not needed 
    the primary keys for this table are test id and question id

--- Backend/Migrations/Test_MetaData.sql
+++ Backend/Migrations/Test_MetaData.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Test_MetaData (
-    Metadata_id SERIAL PRIMARY KEY,
     test_id INT NOT NULL,
     question_id INT NOT NULL,
     points INT DEFAULT 0,
-    order_num INT
+    order_num NOT NULL,
+    PRIMARY KEY (test_id, question_id)
 );
