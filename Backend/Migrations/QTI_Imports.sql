8a866cc204c20ca372fe8161a3a48ae5acb3f4c5
stuckeyrj <65870783+stuckeyrj@users.noreply.github.com>
2025-02-05 14:51:00 -0600

    Update QTI_Imports.sql
    needed to link who imported the test and link it to create test

--- Backend/Migrations/QTI_Imports.sql
+++ Backend/Migrations/QTI_Imports.sql
@@ -2,4 +2,6 @@ CREATE TABLE QTI_Imports (
     import_id SERIAL PRIMARY KEY,
     file_path VARCHAR(255) NOT NULL,
     status VARCHAR(50) CHECK (status IN ('pending', 'processed', 'failed'))
+    owner_id INT NOT NULL
+    test_id INT NOT NULL
     );
