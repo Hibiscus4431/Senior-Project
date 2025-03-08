8eee44acd9d593a2ce4328ff8afdbbb068b8d8b9
lauren35 <lfw0007@uah.edu>
2025-03-08 03:26:24 -0600

    Vue
    Created vue app, started converting HTML pages to vue pages

---
+++ TestCreationVue/my-vue-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Welcome />
+    <WebmasterLog />
+    <WebmasterHome />
+    <TeacherLog />
+    <TeacherHome />
+  </div>
+</template>
+
+<script>
+import Welcome from './components/Welcome.vue';
+import WebmasterLog from './components/WebmasterLog.vue';
+import WebmasterHome from './components/WebmasterHome.vue';
+import TeacherLog from './components/TeacherLog.vue';
+import TeacherHome from './components/TeacherHome.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Welcome,
+    WebmasterLog,
+    WebmasterHome,
+    TeacherLog,
+    TeacherHome
+  }
+}
+</script>
+
+<style>
+/* Global styles can be added here */
+</style>
---
+++ TestCreationVue/my-vue-app/src/components/WebmasterHome.vue
@@ -0,0 +1,31 @@
+<!-- filepath: /c:/Users/laure/Senior-Project/TestCreationVue/src/components/WebmasterHome.vue -->
+ <template>
+      <div class="center large-heading">
+        <h1> Welcome Webmaster</h1>
+    </div>
+
+    <div class="center large-paragraph">
+        View website as:
+        <br><br>
+        <!-- TODO:replace w/ teacher vue homepage-->
+         <router-link to="/Teacher_hp">
+            <button class="button">Teacher</button>
+        </router-link>
+        <br>
+        <br>
+        <!-- TODO: replace w/ publisher vue homepage-->
+        <router-link to="/Pub_hp">
+            <button class="button">Publisher</button>
+        </router-link>
+    </div>
+ </template>
+
+ <script>
+    export default {
+        name: 'WebmasterHome'
+    };
+</script>
+
+<style scoped>
+    @import '../assets/webmaster_styles.css';
+</style>
